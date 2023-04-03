b67d8a8a71dcf7602ebc21a292bd909bd573aca9
adams <stilwelladam8@gmail.com>
2023-04-03 02:15:40 -0400

    Finished the lander code.

--- src/main/java/OnBoardComputer.java
+++ src/main/java/OnBoardComputer.java
@@ -3,7 +3,27 @@ public class OnBoardComputer implements BurnStream {
     @Override
     public int getNextBurn(DescentEvent status) {
         int burn = 0;
-        
+        //4501
+        if((status.getAltitude() - status.getVelocity() < 4501
+                && status.getVelocity() !=100
+                && status.getAltitude() > 100)) {
+            burn = 200;
+        }else if((status.getVelocity() == 100
+                && status.getAltitude()-status.getVelocity()>0)
+                || status.getAltitude() - status.getVelocity() > 4501
+                || status.getVelocity() < status.getAltitude()){
+            burn = 100;
+        }else if(status.getVelocity() == 100
+                && status.getAltitude() <100){
+            burn = 201 - status.getAltitude();
+        }else if(status.getVelocity()<100
+                && status.getAltitude()<100
+                && status.getVelocity()< status.getAltitude()){
+            burn = 100 + status.getAltitude() - status.getVelocity();
+        }else if (status.getVelocity() > status.getAltitude()
+                || status.getVelocity() == status.getAltitude()){
+            burn = 100 + status.getVelocity()-2;
+        }
         System.out.println(burn); /*hack!*/
         return burn;
     }
